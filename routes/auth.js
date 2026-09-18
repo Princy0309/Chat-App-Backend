31f4a68033d90eb33a20340634c8a7877bb92b2b
Princy Mishra <princyyy03@gmail.com>
2026-09-18 08:29:37 +0530

    Add login route to auth

--- routes/auth.js
+++ routes/auth.js
@@ -27,3 +27,30 @@ router.post('/register', async(req, res)=>{
         res.status(500).json({message: 'Server error', error: err.message});
     }
 });
+
+router.post('/login', async(req, res)=>{
+    try{
+        const { email, password } = req.body;
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message : "invalid credentials"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({message: "Invalid credentials"})
+        }
+        const token = jwt.sign(
+            {userId: user._id, username: user.username},
+            process.env.JWT_SECRET,
+            {expiresIn: '7d'}
+        );
+
+        res.json({token, username: user.username});
+
+    }catch(err){
+        res.status(500).json({message: "server error", error: err.message});
+    }
+})
+
+module.exports = router;
