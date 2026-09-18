7ef40a5dd19bf50ff9c491a30ec5fd6e23bcaf23
Princy Mishra <princyyy03@gmail.com>
2026-09-18 11:34:49 +0530

    Fix: change login page at root URL

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 app.get('/', (req, res)=>{
-    res.send("chat app backend is running");
+    res.send(__dirname + '/public/login.html');
 });
 
 io.on('connection', async (socket)=>{
